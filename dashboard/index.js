f272254321a205b61d061cb1a4973b36ab825f0d
kylejohnson <kyle@solidstategroup.com>
2016-08-21 22:07:55 +0100

    Wrote a better cli

--- dashboard/index.js
+++ dashboard/index.js
@@ -7,310 +7,370 @@ var formatOutput = require("../utils/format-output.js");
 var formatModules = require("../utils/format-modules.js");
 var formatAssets = require("../utils/format-assets.js");
 
-function Dashboard(options) {
-  this.color = options && options.color || "green";
-  this.minimal = options && options.minimal || false;
-  this.setData = this.setData.bind(this);
 
-  this.screen = blessed.screen({
-    smartCSR: true,
-    title: "webpack-dashboard",
-    dockBorders: false,
-    fullUnicode: true,
-    autoPadding: true
-  });
+require('colors');
 
-  this.layoutLog.call(this);
-  this.layoutStatus.call(this);
-  !this.minimal && this.layoutModules.call(this);
-  !this.minimal && this.layoutAssets.call(this);
+const bg = 'red';
+const os = require('os');
+const ifaces = os.networkInterfaces();
+const ngrok = require('ngrok');
+const port = process.env.PORT || 8080;
+const getIP = new Promise(function (resolve) {
+    Object.keys(ifaces).forEach(function (ifname) {
+        var alias = 0;
 
-  this.screen.key(["escape", "q", "C-c"], function() {
-    process.exit(0);
-  });
+        ifaces[ifname].forEach(function (iface) {
+            if ('IPv4' !== iface.family || iface.internal !== false) {
+                // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+                return;
+            }
+            if (ifname == 'en0') {
+                resolve('http://' + iface.address);
+            }
+        });
+        // resolve();
+    });
+});
+var ip1 = '';
+var ip2 = '';
+var ip3 = '';
+var style = {
+    fillCellBorders: false
+};
 
-  this.screen.render();
+var status = "";
+var progress = "";
+var operation = '';
+var dash = null;
+var padding = 0;
+
+var updateStatus = function () {
+    var op = '{bold}Operation: {/}'+operation;
+    var stat = '{bold}Status: {/}'+status;
+    var prog = '{bold}Progress: {/}'+progress;
+    dash.status.setContent( stat + '\n' + op + '\n' + prog);
+};
+
+function Dashboard (options) {
+    dash = this;
+    this.color = options && options.color || "green";
+    this.minimal = options && options.minimal || false;
+    this.setData = this.setData.bind(this);
+
+    this.screen = blessed.screen({
+        smartCSR: true,
+        style: style,
+        title: "webpack-dashboard",
+        dockBorders: false,
+        fullUnicode: true,
+    });
+
+    this.layoutLog.call(this);
+    this.layoutStatus.call(this);
+    !this.minimal && this.layoutModules.call(this);
+    !this.minimal && this.layoutAssets.call(this);
+
+    this.screen.key(["escape", "q", "C-c"], function () {
+        process.exit(0);
+    });
+
+
+    ngrok.connect(port, function (innerErr, url) {
+        if (innerErr) {
+            return console.log(innerErr);
+        }
+        getIP
+            .then(function (res) {
+                ip1 = (url).bold.blue;
+                ip2 = ('http://localhost:' + port).bold.green;
+                ip3 = (res + ':' + port).bold.grey;
+                this.ips.log(ip1  + '\n' + ip2 + '\n' + ip3 + '{/}')
+            }.bind(this));
+    }.bind(this));
+
+    updateStatus();
+    this.screen.render();
 }
 
-Dashboard.prototype.setData = function(dataArr) {
-  var self = this;
+Dashboard.prototype.setData = function (dataArr) {
+    var self = this;
 
-  dataArr.forEach(function(data) {
-    switch (data.type) {
-      case "progress": {
-        var percent = parseInt(data.value * 100);
-        if (self.minimal) {
-          percent && self.progress.setContent(percent.toString() + "%");
-        } else {
-          percent && self.progressbar.setContent(percent.toString() + "%");
-          self.progressbar.setProgress(percent);
+    dataArr.forEach(function (data) {
+        switch (data.type) {
+            case "progress": {
+                var percent = parseInt(data.value * 100);
+                if (self.minimal) {
+                    percent && self.progress.setContent(percent.toString() + "%");
+                } else {
+                    percent && self.progressbar.setContent(percent.toString() + "%");
+                    progress = percent;
+                    updateStatus();
+                }
+                break;
+            }
+            case "operations": {
+                operation = data.value;
+                updateStatus();
+                break;
+            }
+            case "status": {
+                var content;
+
+                switch (data.value) {
+                    case "Success":
+                        content = "{green-fg}{bold}" + data.value + "{/}";
+                        break;
+                    case "Failed":
+                        content = "{red-fg}{bold}" + data.value + "{/}";
+                        break;
+                    default:
+                        content = "{bold}" + data.value + "{/}";
+                }
+                status = content;
+                updateStatus();
+                break;
+            }
+            case "stats": {
+                var stats = {
+                    hasErrors: function () {
+                        return data.value.errors;
+                    },
+                    hasWarnings: function () {
+                        return data.value.warnings;
+                    },
+                    toJson: function () {
+                        return data.value.data;
+                    }
+                };
+                if (stats.hasErrors()) {
+                    status = "{red-fg}{bold}Failed{/}";
+                    updateStatus();
+                }
+                self.logText.log(formatOutput(stats));
+                !self.minimal && self.moduleTable.setData(formatModules(stats));
+                !self.minimal && self.assetTable.setData(formatAssets(stats));
+                break;
+            }
+            case "log": {
+                self.logText.log(data.value);
+                break;
+            }
+            case "clear": {
+                self.logText.setContent("");
+                break;
+            }
         }
-        break;
-      }
-      case "operations": {
-        self.operations.setContent(data.value);
-        break;
-      }
-      case "status": {
-        var content;
+    });
 
-        switch(data.value) {
-          case "Success":
-            content = "{green-fg}{bold}" + data.value + "{/}";
-            break;
-          case "Failed":
-            content = "{red-fg}{bold}" + data.value + "{/}";
-            break;
-          default:
-            content = "{bold}" + data.value + "{/}";
-        }
-        self.status.setContent(content);
-        break;
-      }
-      case "stats": {
-        var stats = {
-          hasErrors: function() {
-            return data.value.errors;
-          },
-          hasWarnings: function() {
-            return data.value.warnings;
-          },
-          toJson: function() {
-            return data.value.data;
-          }
-        };
-        if (stats.hasErrors()) {
-          self.status.setContent("{red-fg}{bold}Failed{/}");
-        }
-        self.logText.log(formatOutput(stats));
-        !self.minimal && self.moduleTable.setData(formatModules(stats));
-        !self.minimal && self.assetTable.setData(formatAssets(stats));
-        break;
-      }
-      case "log": {
-        self.logText.log(data.value);
-        break;
-      }
-      case "clear": {
-        self.logText.setContent("");
-        break;
-      }
-    }
-  });
-
-  this.screen.render();
+    this.screen.render();
 };
 
-Dashboard.prototype.layoutLog = function() {
-  this.log = blessed.box({
-    label: "Log",
-    padding: 1,
-    width: this.minimal ? "100%" : "75%",
-    height: this.minimal ? "70%" : "42%",
-    left: "0%",
-    top: "0%",
-    border: {
-      type: "line",
-    },
-    style: {
-      fg: -1,
-      border: {
-        fg: this.color,
-      },
-    },
-  });
+Dashboard.prototype.layoutLog = function () {
+    this.log = blessed.box({
+        label: "Log",
+        width: this.minimal ? "100%" : "75%",
+        height: this.minimal ? "70%" : "42%",
+        left: "0%",
+        top: "0%",
+        border: {
+            type: "line",
+        },
+        style: style
+    });
 
-  this.logText = blessed.log({
-    parent: this.log,
-    tags: true,
-    width: "100%-5",
-    scrollable: true,
-    input: true,
-    alwaysScroll: true,
-    scrollbar: {
-      ch: " ",
-      inverse: true
-    },
-    keys: true,
-    vi: true,
-    mouse: true
-  });
+    this.logText = blessed.log({
+        parent: this.log,
+        tags: true,
+        width: "100%-5",
+        scrollable: true,
+        input: true,
+        alwaysScroll: true,
+        scrollbar: {
+            ch: " ",
+            inverse: true
+        },
+        keys: true,
+        vi: true,
+        mouse: true,
+        style: style
+    });
 
-  this.screen.append(this.log);
+    this.screen.append(this.log);
 };
 
-Dashboard.prototype.layoutModules = function() {
-  this.modules = blessed.box({
-    label: "Modules",
-    tags: true,
-    padding: 1,
-    width: "50%",
-    height: "58%",
-    left: "0%",
-    top: "42%",
-    border: {
-      type: "line",
-    },
-    style: {
-      fg: -1,
-      border: {
-        fg: this.color,
-      },
-    },
-  });
+Dashboard.prototype.layoutModules = function () {
+    this.modules = blessed.box({
+        label: "Modules",
+        tags: true,
+        width: "50%",
+        height: "58%",
+        left: "0%",
+        top: "42%",
+        border: {
+            type: "line",
+        },
+        style: style,
+    });
 
-  this.moduleTable = blessed.table({
-    parent: this.modules,
-    height: "100%",
-    width: "100%-5",
-    align: "left",
-    pad: 1,
-    shrink: true,
-    scrollable: true,
-    alwaysScroll: true,
-    scrollbar: {
-      ch: " ",
-      inverse: true
-    },
-    keys: true,
-    vi: true,
-    mouse: true,
-    data: [["Name", "Size", "Percentage"]]
-  });
+    this.moduleTable = blessed.table({
+        fillCellBorders: true,
+        parent: this.modules,
+        style: style,
+        height: "100%",
+        width: "100%-5",
+        align: "left",
+        pad: 1,
+        shrink: true,
+        scrollable: true,
+        alwaysScroll: true,
+        scrollbar: {
+            ch: " ",
+            inverse: true
+        },
+        keys: true,
+        vi: true,
+        mouse: true,
+        data: [["Name", "Size", "Percentage"]]
+    });
 
-  this.screen.append(this.modules);
+    this.screen.append(this.modules);
 };
 
-Dashboard.prototype.layoutAssets = function() {
-  this.assets = blessed.box({
-    label: "Assets",
-    tags: true,
-    padding: 1,
-    width: "50%",
-    height: "58%",
-    left: "50%",
-    top: "42%",
-    border: {
-      type: "line",
-    },
-    style: {
-      fg: -1,
-      border: {
-        fg: this.color,
-      },
-    },
-  });
+Dashboard.prototype.layoutAssets = function () {
+    this.assets = blessed.box({
+        label: "Assets",
+        tags: true,
+        padding: padding,
+        width: "50%",
+        height: "58%",
+        left: "50%",
+        top: "42%",
+        border: {
+            type: "line",
+        },
+        style: style
+    });
 
-  this.assetTable = blessed.table({
-    parent: this.assets,
-    height: "100%",
-    width: "100%-5",
-    align: "left",
-    pad: 1,
-    scrollable: true,
-    alwaysScroll: true,
-    scrollbar: {
-      ch: " ",
-      inverse: true
-    },
-    keys: true,
-    vi: true,
-    mouse: true,
-    data: [["Name", "Size"]]
-  });
+    this.assetTable = blessed.table({
+        parent: this.assets,
+        style: style,
+        height: "100%",
+        width: "100%-5",
+        align: "left",
+        pad: 1,
+        scrollable: true,
+        alwaysScroll: true,
+        scrollbar: {
+            ch: " ",
+            inverse: true
+        },
+        keys: true,
+        vi: true,
+        mouse: true,
+        data: [["Name", "Size"]]
+    });
 
-  this.screen.append(this.assets);
+    this.screen.append(this.assets);
 };
 
-Dashboard.prototype.layoutStatus = function() {
+Dashboard.prototype.layoutStatus = function () {
 
-  this.wrapper = blessed.layout({
-    width: this.minimal ? "100%" : "25%",
-    height: this.minimal ? "30%" : "42%",
-    top: this.minimal ? "70%" : "0%",
-    left: this.minimal ? "0%" : "75%",
-    layout: "grid"
-  });
+    this.wrapper = blessed.layout({
+        width: this.minimal ? "100%" : "33%",
+        height: this.minimal ? "30%" : "42%",
+        top: this.minimal ? "70%" : "0%",
+        left: this.minimal ? "0%" : "75%",
+        layout: "grid",
+        style: style
+    });
 
-  this.status = blessed.box({
-    parent: this.wrapper,
-    label: "Status",
-    tags: true,
-    padding: {
-      left: 1,
-    },
-    width: this.minimal ? "34%" : "100%",
-    height: this.minimal ? "100%" : "34%",
-    valign: "middle",
-    border: {
-      type: "line",
-    },
-    style: {
-      fg: -1,
-      border: {
-        fg: this.color,
-      },
-    },
-  });
+    this.operations = blessed.box({
+        parent: this.wrapper,
+        label: "Operation",
+        tags: true,
+        padding: padding,
+        width: this.minimal ? "0%" : "100%",
+        height: this.minimal ? "100%" : "34%",
+        valign: "middle",
+        border: {
+            type: "line",
+        },
+        style: style,
+    });
 
-  this.operations = blessed.box({
-    parent: this.wrapper,
-    label: "Operation",
-    tags: true,
-    padding: {
-      left: 1,
-    },
-    width: this.minimal ? "34%" : "100%",
-    height: this.minimal ? "100%" : "34%",
-    valign: "middle",
-    border: {
-      type: "line",
-    },
-    style: {
-      fg: -1,
-      border: {
-        fg: this.color,
-      },
-    },
-  });
+    this.ipBox = blessed.box({
+        parent: this.wrapper,
+        label: "Network",
+        tags: true,
+        padding: padding,
+        width: this.minimal ? "50%" : "100%",
+        height: this.minimal ? "100%" : "34%",
+        valign: "middle",
+        border: {
+            type: "line",
+        },
+        style: style,
+    });
 
-  this.progress = blessed.box({
-    parent: this.wrapper,
-    label: "Progress",
-    tags: true,
-    padding: this.minimal ? {
-      left: 1,
-    } : 1,
-    width: this.minimal ? "33%" : "100%",
-    height: this.minimal ? "100%" : "34%",
-    valign: "middle",
-    border: {
-      type: "line",
-    },
-    style: {
-      fg: -1,
-      border: {
-        fg: this.color,
-      },
-    },
-  });
+    this.status = blessed.box({
+        parent: this.wrapper,
+        label: "Status",
+        tags: true,
+        padding: padding,
+        width: this.minimal ? "50%" : "100%",
+        height: this.minimal ? "100%" : "34%",
+        border: {
+            type: "line",
+        },
+        style: style,
+    });
 
-  this.progressbar = blessed.ProgressBar({
-    parent: this.progress,
-    height: 1,
-    width: "90%",
-    top: "center",
-    left: "center",
-    hidden: this.minimal,
-    orientation: "horizontal",
-    style: {
-      bar: {
-        bg: this.color,
-      },
-    }
-  });
+    this.progress = blessed.box({
+        parent: this.wrapper,
+        label: "Progress",
+        tags: true,
+        padding: padding,
+        width: this.minimal ? "0%" : "100%",
+        height: this.minimal ? "100%" : "34%",
+        valign: "middle",
+        border: {
+            type: "line",
+        },
+        style: style,
+    });
 
-  this.screen.append(this.wrapper);
+
+
+
+    this.ips = blessed.log({
+        parent: this.ipBox,
+        tags: true,
+        width: "100%-5",
+        scrollable: true,
+        input: true,
+        alwaysScroll: true,
+        scrollbar: {
+            ch: " ",
+            inverse: true
+        },
+        keys: true,
+        vi: true,
+        style: style,
+        mouse: true,
+    });
+
+    this.progressbar = blessed.ProgressBar({
+        parent: this.progress,
+        height: 1,
+        width: "90%",
+        top: "center",
+        left: "center",
+        hidden: this.minimal,
+        orientation: "horizontal",
+        style: style
+    });
+
+    this.screen.append(this.wrapper);
 };
 
 module.exports = Dashboard;
